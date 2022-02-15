cf39f3c1e35d67bde5ed9e4d9e016e37bb6d4803
Njazi Shehu <shehu.nj@gmail.com>
2022-02-16 00:06:08 +0100

    removed migration from config file

--- src/BreakingBadServiceProvider.php
+++ src/BreakingBadServiceProvider.php
@@ -18,7 +18,6 @@ class BreakingBadServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-breaking-bad')
             ->hasConfigFile()
-            ->hasMigration('create_laravel-breaking-bad_table')
             ->hasCommand(BreakingBadCommand::class);
     }
 }
